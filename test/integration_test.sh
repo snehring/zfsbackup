682f0e1d5e134af885a440181054856782da0722
snehring <snehring@iastate.edu>
2023-08-22 11:33:02 -0500

    minor change for integration test

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
+POOL=trash
 set -x
 set -e
 bash integration_test-setup.sh
-cp -r /etc/ /store/zfs_backup_test/source
-cp -r /etc/ /store/zfs_backup_test/source2
-python3	zfsbackup.py --config integration_test.yml
+cp -r /etc/ /${POOL}/zfs_backup_test/source
+cp -r /etc/ /${POOL}/zfs_backup_test/source2
+python3	../zfsbackup.py --config integration_test.yml
 sleep 62
-cp -r /usr/bin /store/zfs_backup_test/source
-cp -r /usr/bin /store/zfs_backup_test/source2
-python3 zfsbackup.py --config integration_test.yml
+cp -r /usr/bin /${POOL}/zfs_backup_test/source
+cp -r /usr/bin /${POOL}/zfs_backup_test/source2
+python3 ../zfsbackup.py --config integration_test.yml
 zfs list
 zfs list -t snap
 bash integration_test-teardown.sh
