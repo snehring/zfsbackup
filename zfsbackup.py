fd5bf5506de8687b0dfde3a78186c4eb76f0550c
Shane Nehring <snehring@iastate.edu>
2026-05-21 15:37:49 -0500

    removing -F from recv, adding -o canmount=noauto
    The -F flag on recv can be a footgun, but does make it so that the recv
    will not fail if the recv-ing side has somehow been modified. Since
    these are non-root user recvs they won't(can't) be automounted so they
    shouldn't be modified on the recv-ing side anyway. Adding the -o
    canmount=noauto should stop any mount attempts on initial sends and the
    errors those can cause though.
    
    Signed-off-by: Shane Nehring <snehring@iastate.edu>

--- zfsbackup.py
+++ zfsbackup.py
@@ -581,10 +581,10 @@ def send_snapshot(snapshot, destination, transport="local", incremental_source=N
     """
     logger = logging.getLogger(__name__)
     send_flags = ""
-    recv_flags = "-F"
+    recv_flags = "-o canmount=noauto"
     if is_encrypted_dataset(snapshot):
         send_flags = "-w"
-        recv_flags = ""
+        recv_flags = "-o canmount=noauto"
     elif get_transport_type(transport) == "ssh":
         send_flags = ""
 
